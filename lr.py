3dc52924ff5e774445cebb9b811ad436f8f7b2da
Serge Makhoul <74646466+SergeMakhoul@users.noreply.github.com>
2022-03-14 14:57:37 +0100

    added class for basic linear regression

---
+++ lr.py
@@ -0,0 +1,63 @@
+from time import perf_counter
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+
+
+class LinearRegression():
+    def __init__(self, datatsetPath='dataset/dataset.csv', training_epochs=100, learning_rate=0.01):
+        self.datasetPath = datatsetPath
+        self.x_train = pd.read_csv(self.datasetPath)['X'].to_numpy()
+        self.y_train = pd.read_csv(self.datasetPath)['Y'].to_numpy()
+
+        # Learning rate
+        self.learning_rate = learning_rate
+
+        # Number of loops for training through all your data to update the parameters
+        self.training_epochs = training_epochs
+
+        # declare weights
+        self.weight = tf.Variable(0.)
+        self.bias = tf.Variable(0.)
+
+        # To store the training time
+        self.training_time = 0
+
+    def linreg(self, x):
+        y = self.weight * x + self.bias
+        return y
+
+    def squared_error(self, y_pred, y_true):
+        return tf.reduce_mean(tf.square(y_pred - y_true))
+
+    def fit(self):
+        t_start = perf_counter()
+        for epoch in range(self.training_epochs):
+            # Compute loss within Gradient Tape context
+            with tf.GradientTape() as tape:
+                y_predicted = self.linreg(self.x_train)
+                loss = self.squared_error(y_predicted, self.y_train)
+
+                # Get gradients
+                gradients = tape.gradient(loss, [self.weight, self.bias])
+
+                # Adjust weights
+                self.weight.assign_sub(gradients[0]*self.learning_rate)
+                self.bias.assign_sub(gradients[1]*self.learning_rate)
+
+                # Print output
+                print(f"Epoch count {epoch}: Loss value: {loss.numpy()}")
+
+        self.training_time = perf_counter() - t_start
+
+    def get_weights(self):
+        return (self.weight.numpy(), self.bias.numpy())
+
+    def get_training_time(self):
+        return self.training_time
+
+    def plot(self):
+        plt.scatter(self.x_train, self.y_train)
+        plt.plot(self.x_train, self.linreg(self.x_train), 'r')
+        plt.show()
