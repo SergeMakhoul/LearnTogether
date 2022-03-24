a12179baa6610d3d284f39ead8acd7bcdfb61c81
Serge Makhoul <74646466+SergeMakhoul@users.noreply.github.com>
2022-03-24 17:09:23 +0100

    moved evaluation function

--- utils.py
+++ utils.py
@@ -2,6 +2,10 @@ import os
 import pickle
 from typing import Dict, List
 
+import tensorflow as tf
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 
 def save_history(name: str, history: Dict):
     path: str = f'simulation/{name}'
@@ -20,3 +24,16 @@ def save_history(name: str, history: Dict):
 
     with open(path, 'wb') as f:
         pickle.dump(data, f)
+
+
+def evaluate_model(x, y):
+    directory = 'models'
+
+    dict = {}
+
+    for file in os.listdir(directory):
+        path = os.path.join(directory, file)
+        model = tf.keras.models.load_model(path)
+        dict[file] = model.evaluate(x, y)
+
+    print(dict)
