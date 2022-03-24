0b7104975500d868d4704eea252dbfde1720128f
Serge Makhoul <74646466+SergeMakhoul@users.noreply.github.com>
2022-03-24 16:15:52 +0100

    added file for utility functions

---
+++ utils.py
@@ -0,0 +1,23 @@
+import json
+import os
+import pickle
+from typing import Dict, List
+
+
+def save_history(name: str, history: Dict):
+    path: str = f'simulation/{name}'
+    data: List[Dict] = []
+
+    if not os.path.exists(path):
+        with open(path, 'wb+') as f:
+            data.append(history)
+            pickle.dump(data, f)
+        return
+
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+
+    data.append(history)
+
+    with open(path, 'wb') as f:
+        pickle.dump(data, f)
