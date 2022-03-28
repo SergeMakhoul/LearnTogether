fee1623f4a16a6d8a1287b5bd9aa0cf4bfd1640d
Serge Makhoul <74646466+SergeMakhoul@users.noreply.github.com>
2022-03-28 17:53:34 +0200

    added code to empty old output folder

--- simulate.sh
+++ simulate.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
-if [[ $1 != '' && $1 -gt 0 ]]
+echo 'clearing old outputs'
+rm ./output/*
+rm ./models/*
+rm ./simulation/*
+
+if [[ $1 != '' && $1 -gt 2 ]]
 then
     nb=$1
 else
     nb=20
 fi
 
-for i in `seq 0 ${nb}`; do
-    bash ./run.sh 2
+for i in `seq 0 $(($nb - 1))`; do
+    bash ./run.sh 1
     wait
 done
