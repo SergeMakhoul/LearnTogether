fc60b14fc5421c78feed0a42006aef2b447c52a0
Serge Makhoul <74646466+SergeMakhoul@users.noreply.github.com>
2022-05-03 15:53:57 +0200

    changed the simulation process

--- simulate.sh
+++ simulate.sh
@@ -1,18 +1,37 @@
 #! /bin/bash
 
-echo 'clearing old outputs'
-rm ./output/*
-rm ./models/*
-rm ./simulation/*
-
-if [[ $1 != '' && $1 -gt 2 ]]
+if [[ $1 != '' ]]
 then
     nb=$1
 else
-    nb=20
+    nb=10
 fi
 
-for i in `seq 0 $(($nb - 1))`; do
-    bash ./run.sh 1
+if [[ $2 != '' ]]
+then
+    nb_clients=$2
+else
+    nb_clients=3
+fi
+
+echo 'clearing old outputs...'
+rm ./output/*
+rm ./models/*
+rm ./simulation/*
+rm ./simulation_server/*
+
+for i in `seq 1 $nb`; do
+    echo 'clearing old dataset...'
+    rm ./dataset/*
+    echo 'creating new dataset...'
+    python dataset.py $nb_clients
     wait
+
+    for f in simulation/*; do if [[ "$f" =~ client[[:digit:]]$ ]]; then mv "$f" "$f-$i"; fi; done
+
+    echo 'running simulation...'
+    for j in `seq 1 10`; do
+        bash ./run.sh $nb_clients
+        wait
+    done
 done
