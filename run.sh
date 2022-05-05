06ac82bd1f5ba9bd9cc54f46d70d5142f2144ad5
Serge Makhoul <74646466+SergeMakhoul@users.noreply.github.com>
2022-05-05 19:00:29 +0200

    started running the clients before the server

--- run.sh
+++ run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# echo "Running benchmark simple linear regression code"
-# python lr.py > output/benchmark.txt &
-# wait
-
-echo "Starting server"
-python tfserver.py &
-# sleep 5
-
 if [[ $1 != '' && $1 -gt 0 ]]
 then
     nb=$1
@@ -16,11 +8,13 @@ else
 fi
 
 for i in `seq 0 $(($nb - 1))`; do
-    echo "Starting client $i"
-    mkdir output >/dev/null 2>&1
+    echo "[INFO] Run | Starting client $i"
     python tfclient.py $i > output/client$i.txt &
     # python tfclient.py $i > /dev/null &
 done
+
+echo "[INFO] Run | Starting server"
+python tfserver.py &
 
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 wait
